6a3b3b02f9b8c1daf83b451fc5e88c7d510e8a0e
Matthew Soh <matthewsoh.cw@gmail.com>
2016-08-08 23:44:06 +0800

    366 A, B

---
+++ codeforces/366_2/b.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef pair<pair<int, int>, int> iii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+
+#define INF 2000000000 // 2 billion
+#define MEMSET_INF 127 // about 2B
+#define MEMSET_HALF_INF 63 // about 1B
+
+int N;
+
+int winner = 1;
+
+int main(){
+  scanf("%d", &N);
+  for(int i = 0; i < N; ++i){
+    int a;
+    scanf("%d", &a);
+    if(a%2 == 0) winner = 1-winner;
+    printf("%d\n", winner+1);
+  }
+}
